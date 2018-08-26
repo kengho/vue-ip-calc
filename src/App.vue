0f6cde1f9b65d427cd8b9427023d7c542fb48735
Alexander Morozov <ntcomp12@gmail.com>
2018-08-26 20:15:29 +0300

    added github mark

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
     <IpCalc />
+    <GithubMark
+      href="https://github.com/kengho/vue-ip-calc"
+      position="bottom-right"
+    />
   </div>
 </template>
 
 <script>
+import GithubMark from 'vue-github-mark'
 import IpCalc from './components/IpCalc.vue'
 
 export default {
   name: 'app',
   components: {
     IpCalc,
+    GithubMark,
   },
   created() {
     document.title = "IP Calc";
